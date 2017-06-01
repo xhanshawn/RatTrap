e8337c9df40e681e3078504a921f835874f55e3c
xhanshawn <xhan@wpi.edu>
2017-06-01 13:56:20 +0000

    init commit. - add a module RatTrap to unpack message pack file and run passed in unix command on the unpacked file. - add a method to unpack message pack file. - add an executable file to make a cli tool.

---
+++ lib/rat_trap.rb
@@ -0,0 +1,68 @@
+require "rat_trap/version"
+require 'msgpack'
+
+module RatTrap
+
+  TEMP_DIR = "/tmp/rat_trap"
+  TEMPFILE_TEMPLATE = "%s.unpacked"
+  MSGPACK_FILE_EXT = /.bin/
+
+  def self.run(args, options)
+    file_idx = args.find_index { |arg| arg =~ MSGPACK_FILE_EXT }
+    unless file_idx
+      puts "please specify a MessagePack(bin) file"
+      return
+    end
+    file = args[file_idx]
+    args[file_idx] = '%s'
+    cmd = args.join(" ")
+    if unpack(file, options)
+      tempfile = TEMPFILE_TEMPLATE % File.basename(file)
+      path = temp_path(tempfile)
+      system(cmd % path)
+    end
+  end
+
+  def self.unpack_here(args, options = {})
+    file_idx = args.find_index { |arg| arg =~ MSGPACK_FILE_EXT }
+    unless file_idx
+      puts "please specify a MessagePack(bin) file"
+      return
+    end
+    file = args[file_idx]
+    unpack(file, retain: true, here: true)
+  end
+
+  def unpack(file, options = {})
+    unless File.exist?(file)
+      puts "file doesn't exist"
+      return
+    end
+    puts "force to unpack rat file" if options[:force]
+    basename = File.basename(file)
+    tempfile = TEMPFILE_TEMPLATE % basename
+    # return true if File.exist?(tempfile) && !options[:force]
+    path =
+      if options[:here]
+        tempfile
+      else
+        temp_path(tempfile)
+      end
+    File.open(path, "wb") do |f|
+      rat = MessagePack.unpack(File.read(file))
+      data = rat.map(&:to_s).join("\n") + "\n"
+      f.write(data)
+    end
+  ensure
+    FileUtils.rm_rf(tempfile) unless options[:retain]
+  end
+
+  def temp_path(file)
+    FileUtils.mkdir_p(TEMP_DIR)
+    File.join(TEMP_DIR, file)
+  end
+
+  def self.hello
+    puts "hello world"
+  end
+end
---
+++ lib/rat_trap/version.rb
@@ -0,0 +1,3 @@
+module RatTrap
+  VERSION = "0.1.0"
+end
